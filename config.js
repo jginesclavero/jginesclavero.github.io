5af94eb1616b99d8b2ad876a80389d0c4424f005
jginesclavero <jginesclavero@gmail.com>
2020-05-24 19:01:05 +0200

    Update colors

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
   manifestName: 'Resume',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
+  manifestBackgroundColor: ' #71e88a',
+  manifestThemeColor: '#71e88a',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: ``, // This path is subpath of your hosting https://domain/portfolio
@@ -20,7 +20,7 @@ module.exports = {
     {
       icon: 'fa-linkedin-in',
       name: 'Linkedin',
-      url: 'https://linkedin.com/in/theanubhav/',
+      url: 'https://linkedin.com/in/jginesclavero/',
     },
     {
       icon: 'fa-twitter',
