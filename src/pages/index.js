bb5e1a3269559506a522ece513f21defcf8efb94
jginesclavero <jginesclavero@gmail.com>
2020-05-25 19:57:54 +0200

    rm orcid link

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,15 +20,6 @@ const IndexPage = () => (
           </h1>
           <div className="subheading mb-5">
             <a href={`mailto:${config.email}`}>{config.email}</a> ·
-            {/* 
-            <a itemprop="sameAs" content="https://orcid.org/0000-0002-7319-098X" 
-              href="https://orcid.org/0000-0002-7319-098X" target="orcid.widget" 
-              rel="me noopener noreferrer" style="vertical-align:top;">
-                <img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" 
-                  style="width:1em;margin-right:.5em;" alt="ORCID iD icon"/>
-              https://orcid.org/0000-0002-7319-098X
-            </a>
-            */}
           </div>
           <p className="lead mb-5">
             I am experienced in robotic software development with ROS and ROS2. 
