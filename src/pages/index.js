11d97f1ef4a4b30904a27507e7e4da2047cd7976
jginesclavero <jginesclavero@gmail.com>
2020-05-30 19:56:14 +0200

    Added insterests

--- src/pages/index.js
+++ src/pages/index.js
@@ -261,20 +261,15 @@ const IndexPage = () => (
       >
         <div className="w-100">
           <h2 className="mb-5">Interests</h2>
-          {/*
           <p>
-            Apart from being a web developer, I enjoy most of my time being
-            outdoors. In the winter, I am an avid skier and novice ice climber.
-            During the warmer months here in Colorado, I enjoy mountain biking,
-            free climbing, and kayaking.
+            Apart from being a software developer, I enjoy most of my time with
+            my different passions. I am an aspiring chef and I enjoy with the Batch Cooking.
+            I have a 3D printer and I like to do awesome printed life hacks.
+            I love dive with my wife enjoying the life and the peace of the deeps.
+
+            When I meet with my friends, We usually play board games and take
+            some craft beers. 
           </p>
-          <p className="mb-0">
-            When forced indoors, I follow a number of sci-fi and fantasy genre
-            movies and television shows, I am an aspiring chef, and I spend a
-            large amount of my free time exploring the latest technology
-            advancements in the front-end web development world.
-          </p>
-          */}
         </div>    
       </section>
 
